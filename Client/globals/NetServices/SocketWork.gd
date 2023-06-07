cfe1ba16c046b11f23b0d6fc4fecea4af6cd2383
Artem Agaev <47793047+ArtyoamArag@users.noreply.github.com>
2023-06-08 04:33:06 +0500

    Add register user command. Clean up socket work.

--- Client/globals/NetServices/SocketWork.gd
+++ Client/globals/NetServices/SocketWork.gd
@@ -6,50 +6,83 @@ var url:String = 'ws://example.com'
 
 var socket := WebSocketPeer.new()
 
+var Opened:bool = false
 var Connected:bool = false
 
-signal Command(command:String, data:Dictionary)
+var Username:String = ''
+var UUID:String = ''
+
+signal Command(command:String, data)
 
 
+func OpenConnection(username:String)->void:
+	Opened = true
+	Username = username
+	_ConnectSocket()
 
-func _ready():
-	OpenConnection()
+func CloseConnection()->void:
+	Opened = false
+	socket.close()
+
+func SendCommand(command:String, data) -> void:
+	var sendingObject = JSON.stringify({
+		'command': command,
+		'data': data
+	})
+	socket.send_text(sendingObject)
+
 
 func _process(_delta):
+	if not Opened:
+		return
+	
 	socket.poll()
 	
-	if ConnectionIsOpened():
+	if _ConnectionIsOpened():
 		if not Connected: Connected = true
-		GetPackets()
+		_GetPackets()
 	
-	elif ConnectionIsClosed():
-		ConnectionJustClosed()
-		OpenConnection()
+	elif _ConnectionIsClosed():
+		_ConnectionJustClosed()
+		_ConnectSocket()
 
 
-func EmitCommand(command:String, data:Dictionary = {}):
-	emit_signal('Command', command, data)
-	if DEBUG:
-		print("Command: ", command)
-
-func OpenConnection():
+func _ConnectSocket()->void:
 	socket.connect_to_url(url)
+	_ReadUUID()
+	SendCommand('user-registration', {
+		'name':Username,
+		'uuid':UUID
+	})
 
-func GetPackets():
+func _GetPackets():
 	while socket.get_available_packet_count():
 		var packet = socket.get_packet()
-		var command:String = packet.get_string_from_utf8()
-		EmitCommand(command)
+		var packetString:String = packet.get_string_from_utf8()
+		var commandObject = JSON.parse_string(packetString)
+		_EmitCommand(commandObject['command'], commandObject['data'])
 
-func ConnectionJustClosed():
+func _ConnectionJustClosed():
 	if Connected:
 		Connected = false
 		var code = socket.get_close_code()
 		var reason = socket.get_close_reason()
-		EmitCommand('ConnectionClosed', {'code':code, 'reason':reason})
+		_EmitCommand('conn-closed', {'code':code, 'reason':reason})
 
-func ConnectionIsOpened() -> bool:
+func _EmitCommand(command:String, data):
+	emit_signal('Command', command, data)
+
+
+
+func _ConnectionIsOpened() -> bool:
 	return socket.get_ready_state() == WebSocketPeer.STATE_OPEN
 
-func ConnectionIsClosed() -> bool:
+func _ConnectionIsClosed() -> bool:
 	return socket.get_ready_state() == WebSocketPeer.STATE_CLOSED
+
+func _ReadUUID() -> void:
+	var cookies = JavaScriptBridge.eval('document.cookie;') as String
+	var start = cookies.find('uuid=')
+	var end = cookies.find(';', start)
+	UUID = cookies.substr(start, end-start).split('=')[1]
+
