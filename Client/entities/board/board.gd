06289fa2d1de171d5dea02651882c37ea9c18f5d
Vikktoria6 <vgustokashina@gmail.com>
2023-06-26 21:06:02 +0500

    add change in function

--- Client/entities/board/board.gd
+++ Client/entities/board/board.gd
@@ -1,9 +1,15 @@
 extends Node2D
 
+@export var field_size = 15
+@export var field_tile = Vector2i (5, 12)
+
 signal click_coord 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	pass # Replace with function body.
+	var tls = get_child(0)
+	for i in range(0, field_size):
+		for j in range(0, field_size):
+				tls.set_cell(0, Vector2i (i, j), 1, field_tile)
 
 # словарь
 # тут должны быть координаты тайлов с соответствкующими буквами 
@@ -17,14 +23,13 @@ func setChips(coord:Vector2i, letter):       #установка фишки по
 	var tls = get_child(0)
 	var tileCoord = letterTiles[letter]
 	tls.set_cell(0, coord, 1, tileCoord)
-	pass
 
 func _input(event):
 	var tls = get_child(0)
-	if Input.is_action_pressed("Click"):
+	if Input.is_action_just_pressed("Click"):
 		var coord = tls.local_to_map(event.position)
-		click_coord.emit(coord)
-		setChips(coord, "C")
+		if (coord.x < field_size and coord.y < field_size):
+			click_coord.emit(coord)
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
